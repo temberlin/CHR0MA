9b0eeeb208a56801cb9ae788603eaef722719d19
Russell Greene <russellgreene8@gmail.com>
2025-12-29 21:36:25 -0700

    bump version to 1.2

--- CHR0MA/Skeleton.h
+++ CHR0MA/Skeleton.h
@@ -62,7 +62,7 @@ typedef short int			int16;
 /* Versioning information */
 
 #define	MAJOR_VERSION	1
-#define	MINOR_VERSION	1
+#define	MINOR_VERSION	2
 #define	BUG_VERSION		0
 #define	STAGE_VERSION	PF_Stage_DEVELOP
 #define	BUILD_VERSION	1
